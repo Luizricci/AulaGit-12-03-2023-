a35f6afc97c6fba427712a1f0e6ed0133a545829
luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-12 09:50:15 -0300

    console log no for

--- av1.js
+++ av1.js
@@ -9,10 +9,9 @@ if (tabuada <= 0) {
 } else {
     // neste for estamos fazendo o looping para o limite ser igual ao pedido 
     for (let i = 1; i <= limite; i++) {
+        console.log(i)
          // neste console log fizemos a multiplicação e a exibição da tabuada
         console.log(tabuada + i + tabuada * i);
     }
 }
 
-console.log('teste');
-console.log(("nova exibiçaõ"))
