f6c37b67c3086105a0e51202c601d8eee5421123
luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-12 09:28:38 -0300

    portugues mais formal

--- av1.js
+++ av1.js
@@ -4,7 +4,7 @@ let limite = 15;
 if (tabuada <= 0) {
     console.log("nao podemos fazer a mutiplicação por numeros menores ou iguais a 0 ")
 } else if (tabuada % 1 !== 0 || limite % 1 !== 0) {
-    console.log("não multiplicamos numero com virgula")
+    console.log("o fator nao pode conter numeros decimais ")
 } else {
     for (let i = 1; i <= limite; i++) {
         console.log(tabuada + "x" + i + "=" + tabuada * i);
