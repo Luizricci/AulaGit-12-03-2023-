d4eeb2919da58713bc4c2f333856c1438b8cc9dd
luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-12 09:35:16 -0300

    coloquei anotaçoes no codigo

--- av1.js
+++ av1.js
@@ -1,12 +1,15 @@
 let tabuada = 34;
 let limite = 30;
-
+// esse if serve para verificar se o numero é menor ou igual a 0
 if (tabuada <= 0) {
     console.log("nao podemos fazer a mutiplicação por numeros menores ou iguais a 0 ")
+    // no else if estamos verificando se o numero tem casas decimais 
 } else if (tabuada % 1 !== 0 || limite % 1 !== 0) {
     console.log("o fator nao pode conter numeros decimais ")
 } else {
+    // neste for estamos fazendo o looping para o limite ser igual ao pedido 
     for (let i = 1; i <= limite; i++) {
+         // neste console log fizemos a multiplicação e a exibição da tabuada
         console.log(tabuada + "x" + i + "=" + tabuada * i);
     }
 }
